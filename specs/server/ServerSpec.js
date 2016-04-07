f1f566eac808502f1c4ea5077203a8e28aa25a31
Ryan Haase <ryandhaase@gmail.com>
2016-04-07 18:12:58 -0400

    Move trips model to own folder

--- specs/server/ServerSpec.js
+++ specs/server/ServerSpec.js
@@ -2,4 +2,4 @@ var express = require('express');
 var expect = require('chai').expect;
 var app = require('../server.js');
 
-// Cannot write specs until DB is set up and can ping those routes
+// Cannot write specs until DB controller is set up and can ping those routes
