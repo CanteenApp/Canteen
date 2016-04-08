1f13de6edf3881903fd7e74c8fcc88b1f24494b8
Rob Bruhn <rdbruhn@gmail.com>
2016-04-08 13:58:40 -0500

    Create canteen app and ui-router implementation (#45)
    * Create canteen.js file for main app
    *Add ui-bootstrap and ui-router dependencies
    *Change scripts to non-min versions for development
    
    * Add ui-router config, commented out till partials are made
    *Update script tags and changed to ng-view to ui-view
    
    * fix ui-router to be ui.router

---
+++ public/client/canteen.js
@@ -0,0 +1,21 @@
+angular.module('canteen',[
+  // 'canteen.tripForm',
+  'ui.bootstrap',
+  'ui.router'
+]);
+// .config(function($stateProvider, $urlRouterProvider) {
+
+//   $urlRouterProvider.otherwise("/tripForm");
+
+//   $stateProvider
+//     .state('tripForm', {
+//       url: "/tripForm",
+//       templateUrl: "public/client/trip-form/tripForm.html",
+//       controller : tripForm
+//     });
+    // .state('tripForm.lists', {
+    //   url: "/lists",
+    //   templateUrl: "public/client/trip-form/tripFormLists.html",
+    //   controller: TBD
+    // });
+// });
