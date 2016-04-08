5e8bd0bc2ecca1574f5581481744e64e94f9ee68
Rob Bruhn <rdbruhn@gmail.com>
2016-04-08 17:30:13 -0500

    Create landing page with banner image *Add img file and banner image *Modify paths of dependencies *Create landing page controller *Update canteen.js to include landingPage controller and route to landingPage state *Update style file with banner class *Update path in middleware

--- public/client/canteen.js
+++ public/client/canteen.js
@@ -1,21 +1,26 @@
-angular.module('canteen',[
-  // 'canteen.tripForm',
+angular.module('canteen', [
+  'canteen.landingPage',
   'ui.bootstrap',
   'ui.router'
-]);
-// .config(function($stateProvider, $urlRouterProvider) {
+])
+.config(function($stateProvider, $urlRouterProvider) {
 
-//   $urlRouterProvider.otherwise("/tripForm");
+  $urlRouterProvider.otherwise("/");
 
-//   $stateProvider
-//     .state('tripForm', {
-//       url: "/tripForm",
-//       templateUrl: "public/client/trip-form/tripForm.html",
-//       controller : tripForm
-//     });
+  $stateProvider
+        .state('landingPage', {
+      url: "/",
+      templateUrl: "client/landingPage/landingPage.html",
+      controller : 'landingPage'
+    });
+    // .state('tripForm', {
+    //   url: "/tripForm",
+    //   templateUrl: "public/client/trip-form/tripForm.html",
+    //   controller : tripForm
+    // });
     // .state('tripForm.lists', {
     //   url: "/lists",
     //   templateUrl: "public/client/trip-form/tripFormLists.html",
     //   controller: TBD
     // });
-// });
+});
--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -4,5 +4,5 @@ var bodyParser = require('body-parser');
 module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../public/client'));
+  app.use(express.static(__dirname + '/../../public'));
 };
