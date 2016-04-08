e4669914471a050a35989b2462eb9cb142787c55
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-08 17:04:44 -0500

    Add scope objects for the form and members

--- public/client/trip-form/tripForm.js
+++ public/client/trip-form/tripForm.js
@@ -1,3 +1,18 @@
 angular.module('canteen.tripForm', [])
 
-.controller('tripFormData', ['$scope', function($scope) {}])
+.controller('tripFormData', ['$scope', function($scope) {
+    $scope.tripForm = {
+    tripName: '',
+    members: [],
+    location: '',
+    dates: {
+      start: '',
+      end: ''
+  };
+  $scope.tripMember = {
+    email: ''
+  };
+  $scope.addMember = function() {};
+  $scope.createTrip = function() {};
+  };
+}])
