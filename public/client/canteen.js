2caed7fe6407499485b4e2009bf85c04d7a26df8
Rob Bruhn <rdbruhn@gmail.com>
2016-04-08 21:21:46 -0500

    Create Landing page, state, and controller (#53)
    * Create landingPage, controller, and main css file
    
    * Create landing page with banner image
    *Add img file and banner image
    *Modify paths of dependencies
    *Create landing page controller
    *Update canteen.js to include landingPage controller and route to landingPage state
    *Update style file with banner class
    *Update path in middleware
    
    * Add new temp banner image
    
    * Update height of banner image
    
    * Remove whitespace
    
    * Fix indentation

--- public/client/canteen.js
+++ public/client/canteen.js
@@ -1,21 +1,24 @@
-angular.module('canteen',[
-  // 'canteen.tripForm',
+angular.module('canteen', [
+  'canteen.landingPage',
   'ui.bootstrap',
   'ui.router'
-]);
-// .config(function($stateProvider, $urlRouterProvider) {
-
-//   $urlRouterProvider.otherwise("/tripForm");
-
-//   $stateProvider
-//     .state('tripForm', {
-//       url: "/tripForm",
-//       templateUrl: "public/client/trip-form/tripForm.html",
-//       controller : tripForm
-//     });
+])
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('landingPage', {
+      url: "/",
+      templateUrl: "client/landingPage/landingPage.html",
+      controller : 'landingPage'
+    });
+    // .state('tripForm', {
+    //   url: "/tripForm",
+    //   templateUrl: "public/client/trip-form/tripForm.html",
+    //   controller : tripForm
+    // });
     // .state('tripForm.lists', {
     //   url: "/lists",
     //   templateUrl: "public/client/trip-form/tripFormLists.html",
     //   controller: TBD
     // });
-// });
+});
