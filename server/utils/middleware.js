7684c7628ed0ee27221a7e05cba0004aa76638bb
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-11 16:48:39 -0500

    Add sessions and grant to middleware

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,7 +1,13 @@
 var bodyParser = require('body-parser');
+var session = require('exress-session');
+var Grant = require('grant-express');
+
+var grant = new Grant(require('../.config/.grantConfig.js'));
 
 module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  app.use(session(require('../.config/.sessionSecret.json')))
+  app.use(grant);
   app.use(express.static(__dirname + '/../../public'));
 };
