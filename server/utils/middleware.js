ccb11abc48b40f8a42b4fa3a497bd170b8a91f22
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-12 14:30:37 -0500

    Add missing semicolon

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -7,7 +7,7 @@ var grant = new Grant(require('../.config/.grantConfig.json')[process.env.NODE_E
 module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(session(require('../.config/.sessionSecret.json')))
+  app.use(session(require('../.config/.sessionSecret.json')));
   app.use(grant);
   app.use(express.static(__dirname + '/../../public'));
 };
