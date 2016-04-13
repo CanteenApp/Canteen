a8bb69556e64b09af00af54ba548ae637b7cf06c
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-13 13:06:28 -0500

    Add listView to canteen.js and clean up quotes

--- public/client/canteen.js
+++ public/client/canteen.js
@@ -1,20 +1,27 @@
 angular.module('canteen', [
   'canteen.landingPage',
+  'canteen.tripForm',
+  'canteen.listView',
   'ui.bootstrap',
   'ui.router'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/landing-page");
+  $urlRouterProvider.otherwise('/landing-page');
   $stateProvider
     .state('landingPage', {
-      url: "/landing-page",
-      templateUrl: "client/landingPage/landingPage.html",
+      url: '/landing-page',
+      templateUrl: 'client/landingPage/landingPage.html',
       controller : 'landingPage'
     })
     .state('tripForm', {
-      url: "/trip-form",
-      templateUrl: "client/trip-form/tripForm.html",
+      url: '/trip-form',
+      templateUrl: 'client/trip-form/tripForm.html',
       controller : 'tripForm'
+    })
+    .state('listView', {
+      url: '/list-view',
+      templateUrl: 'client/list-view/listView.html',
+      controller: 'listView'
     });
     // .state('tripForm.lists', {
     //   url: "/lists",
