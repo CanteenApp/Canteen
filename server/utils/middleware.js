d6d4209f7f69c714dbedcbeb5ee50e0aa320c22d
Jak Buttarazzi <jakrazzi@gmail.com>
2016-04-13 10:08:11 -0500

    Move grant config into middleware and create variable environ (#84)

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -2,12 +2,32 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var Grant = require('grant-express');
 
-var grant = new Grant(require('../.config/.grantConfig.json')[process.env.NODE_ENV || 'development']);
+var grant = new Grant({
+  "server": {
+    "protocol": "http",
+    "host": process.env.HOST || 'localhost:3333',
+    "callback": "/callback",
+    "transport": "session",
+    "state": true
+  },
+  "google": {
+    "key": process.env.GOOGLE_CID || require('../.config/.secrets.json')['google']['clientId'],
+    "secret": process.env.GOOGLE_CSECRET || require('../.config/.secrets.json')['google']['clientSecret'],
+    "scope": [
+      "https://www.googleapis.com/auth/plus.login",
+      "https://www.googleapis.com/auth/plus.me",
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ]
+  }
+});
 
 module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(session(require('../.config/.sessionSecret.json')));
+  app.use(session({
+    "secret": process.env.SESSION_SECRET || require('../.config/.secrets.json')['session']['secret']
+  }));
   app.use(grant);
   app.use(express.static(__dirname + '/../../public'));
 };
