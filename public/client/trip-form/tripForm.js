ae3f9f6a7584274012aca1527230a7dc3ef3399d
Ryan Haase <ryandhaase@gmail.com>
2016-04-13 12:11:39 -0400

    Set mangle to false in gruntfile

--- public/client/trip-form/tripForm.js
+++ public/client/trip-form/tripForm.js
@@ -2,52 +2,28 @@ angular.module('canteen.tripForm', [
  'canteen.forms',
 ])
 
-// .controller('tripFormCtrl', [
-//   '$scope',
-//   'formFactory',
-//   function ($scope, formFactory) {
-//     $scope.tripForm = {
-//     tripName: '',
-//     members: [],
-//     location: '',
-//     dates: {
-//       start: '',
-//       end: ''
-//     }
-//   };
-//   $scope.tripMember = {
-//     email: ''
-//   };
-//   $scope.addMember = function() {
-//     $scope.tripFrom.members.push($scope.tripMember);
-//     $scope.tripMember = {};
-//   };
-//   $scope.createTrip = function() {
-//     formFactory.submitTrip($scope.tripForm);
-//   };
-// }]);
-.controller('tripFormCtrl', ['$scope', 'formFactory', tripFormCtrl]);
-
-function tripFormCtrl($scope, formFactory) {
-  $scope.tripForm = {
-    tripName: '',
-    members: [],
-    location: '',
-    dates: {
-      start: '',
-      end: '',
-    },
-  };
-  $scope.tripMember = {
-    email: '',
-  };
-
-  $scope.addMember = function () {
-    $scope.tripFrom.members.push($scope.tripMember);
-    $scope.tripMember = {};
-  };
-
-  $scope.createTrip = function () {
-    formFactory.submitTrip($scope.tripForm);
-  };
-};
+.controller('tripFormCtrl', [
+  '$scope',
+  'formFactory',
+  function ($scope, formFactory) {
+    $scope.tripForm = {
+      tripName: '',
+      members: [],
+      location: '',
+      dates: {
+        start: '',
+        end: '',
+      },
+    };
+    $scope.tripMember = {
+      email: ''
+    };
+    $scope.addMember = function() {
+      $scope.tripFrom.members.push($scope.tripMember);
+      $scope.tripMember = {};
+    };
+    $scope.createTrip = function() {
+      formFactory.submitTrip($scope.tripForm);
+    };
+  }
+]);
