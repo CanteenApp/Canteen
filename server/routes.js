2ca666879b350bf9ba120ca59353c0c2e8157c95
Rob Bruhn <rdbruhn@gmail.com>
2016-04-13 14:02:32 -0500

    Create routes for authentication, remove list routes, and update package.json

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 var tripsController = require('./trips/tripsController');
-var listController = require('./lists/listController');
 var taskController = require('./tasks/taskController');
+var userController = require('./users/userController');
 
 var sendResponse = function (res, err, data, status) {
     if (err) {
@@ -10,9 +10,28 @@ var sendResponse = function (res, err, data, status) {
     }
 };
 
-module.exports = function (app, express) {
+var createSession = function (req, res, newUser) {
+  return req.session.regenerate(function () {
+    req.session.user = newUser;
+    res.redirect('/');
+  });
+};
+
+var isLoggedIn = function (req){
+  return req.session ? !!req.session.user : false;
+};
+
+var checkUser = function (req, res, next) {
+  if (isLoggedIn(req)) {
+    next();
+  } else {
+    res.redirect('/landingPage');
+  }
+};
+
+module.exports = function (app) {
   //trip routes
-  app.route('/api/trips')
+  app.route('/api/trips', checkUser)
     .get(function (req, res) {
       tripsController.getAllTrips(function (err, data) {
         sendResponse(res, err, data, 200);
@@ -24,7 +43,7 @@ module.exports = function (app, express) {
       });
     });
 
-  app.route('/api/trips/:_id')
+  app.route('/api/trips/:_id', checkUser)
     .get(function (req, res) {
       tripsController.getTrip(req, function (err, data) {
         sendResponse(res, err, data, 200);
@@ -35,32 +54,9 @@ module.exports = function (app, express) {
         sendResponse(res, err, data, 200);
       });
     });
-  //list routes
-  app.route('/api/lists')
-    .get(function (req, res) {
-      listController.getAllLists(req, function (err, data) {
-        sendResponse(res, err, data, 200);
-      });
-    })
-    .post(function (req, res) {
-      listController.createList(req, function (err, data) {
-        sendResponse(res, err, data, 201);
-      });
-    });
-
-    app.route('/api/lists/:_id')
-      .get(function (req, res) {
-        listController.getList(req, function (err, data) {
-          sendResponse(res, err, data, 200);
-        });
-      })
-      .put(function (req, res) {
-        listController.updateList(req, function (err, data) {
-          sendResponse(res, err, data, 200);
-        });
-      });
+ 
     //tasks routes
-    app.route('/api/tasks')
+    app.route('/api/tasks', checkUser)
       .get(function (req, res) {
         taskController.getTrip(req, function (err, data) {
           sendResponse(res, err, data, 200);
@@ -72,7 +68,7 @@ module.exports = function (app, express) {
         });
       });
 
-    app.route('/api/tasks/:_id')
+    app.route('/api/tasks/:_id', checkUser)
       .get(function (req, res) {
         taskController.getTrip(req, function (err, data) {
           sendResponse(res, err, data, 200);
@@ -83,5 +79,33 @@ module.exports = function (app, express) {
           sendResponse(res, err, data, 200);
         });
       });
-    //TODO:Add User routes
+      //user routes & authentication
+
+      app.route('/api/auth')
+        .post(function (req, res) {
+          userController.signIn(req, function (err, data) {
+            if(err){
+              sendResponse(res, err, null, null);
+            }else{
+              createSession(req, res, data);
+            }
+          });
+        })
+        .put(function (req, res) {
+          userController.signUp(req, function (err, data) {
+            if(err){
+              sendResponse(res, err, null, null);
+            }else{
+              createSession(req, res, data);
+            }
+          });
+        });
+      //handle successful OAuth calls
+      app.route('/callback')
+        .get(function(req, res){
+          if(req.query){
+            //TODO: Create methods to store user data from req.query
+            createSession(req, res, req.query);
+          }
+        });
 };
