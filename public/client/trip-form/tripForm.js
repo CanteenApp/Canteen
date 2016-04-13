1a97d0cea230c498c1a7fc0d3b299cf4ddb90613
Ryan Haase <ryandhaase@gmail.com>
2016-04-13 11:57:53 -0400

    Testing new controller layout to prevent vars from being uglified

--- public/client/trip-form/tripForm.js
+++ public/client/trip-form/tripForm.js
@@ -1,28 +1,53 @@
 angular.module('canteen.tripForm', [
- 'canteen.forms'
+ 'canteen.forms',
 ])
 
-.controller('tripFormCtrl', [
-  '$scope',
-  'formFactory',
-  function ($scope, formFactory) {
-    $scope.tripForm = {
+// .controller('tripFormCtrl', [
+//   '$scope',
+//   'formFactory',
+//   function ($scope, formFactory) {
+//     $scope.tripForm = {
+//     tripName: '',
+//     members: [],
+//     location: '',
+//     dates: {
+//       start: '',
+//       end: ''
+//     }
+//   };
+//   $scope.tripMember = {
+//     email: ''
+//   };
+//   $scope.addMember = function() {
+//     $scope.tripFrom.members.push($scope.tripMember);
+//     $scope.tripMember = {};
+//   };
+//   $scope.createTrip = function() {
+//     formFactory.submitTrip($scope.tripForm);
+//   };
+// }]);
+.controller('tripFormCtrl', ['$scope', 'formFactory', tripFormCtrl]);
+
+function tripFormCtrl($scope, formFactory) {
+  $scope.tripForm = {
     tripName: '',
     members: [],
     location: '',
     dates: {
       start: '',
-      end: ''
-    }
+      end: '',
+    },
   };
   $scope.tripMember = {
-    email: ''
+    email: '',
   };
-  $scope.addMember = function() {
+
+  $scope.addMember = function () {
     $scope.tripFrom.members.push($scope.tripMember);
     $scope.tripMember = {};
   };
-  $scope.createTrip = function() {
+
+  $scope.createTrip = function () {
     formFactory.submitTrip($scope.tripForm);
   };
-}]);
+};
