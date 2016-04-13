ba522f00506045b72877676ff54ca736416fb2aa
Rob Bruhn <rdbruhn@gmail.com>
2016-04-13 17:18:33 -0500

    Implement addTask, removeTask, assignTask, and unassignTask *Change _id to tripId for consistency

--- server/trips/tripsController.js
+++ server/trips/tripsController.js
@@ -16,16 +16,72 @@ module.exports = {
 
   updateTrip: function (req, next) {
     var options = { new: true };
-    Trip.findByIdAndUpdate(req.params._id, req.body, options)
+    Trip.findByIdAndUpdate(req.params.tripId, req.body, options)
       .exec(function (err, trip) {
         next(err, trip);
       });
   },
 
   getTrip: function (req, next) {
-    Trip.findById(req.params._id)
+    Trip.findById(req.params.tripId)
       .exec(function (err, trip) {
         next(err, trip);
       });
   },
+
+  addTask: function (req, next) {
+    Trip.findByIdAndUpdate(req.params.tripId, {
+      $push: {
+        "tasks": req.body
+      }
+    },
+    {
+      safe: true,
+      upsert: true
+    })
+      .exec(function (err, trip) {
+        next(err, trip);
+      });
+  },
+
+  removeTask: function (req, next) {
+    Trip.findByIdAndUpdate(req.params.tripId, {
+      $pull: {
+        "tasks": { _id: req.params.taskId }
+      }
+    })
+      .exec(function (err, trip) {
+        next(err, trip);
+      });
+  },
+
+  //TODO: Create ability to update tasks
+  // updateTask: function (req, next) {
+  // },
+
+  assignTask: function (req, next) {
+    Trip.findByIdAndUpdate(req.params.tripId, {
+      $push: {
+        "assignedTo": req.body
+      }
+    },
+    {
+      safe: true,
+      upsert: true
+    })
+      .exec(function (err, trip) {
+        next(err, trip);
+      });
+  },
+
+  unassignTask: function (req, next) {
+    Trip.findByIdAndUpdate(req.params.tripId, {
+      $pull: {
+        "assignedTo": { _id: req.params.memberId }
+      }
+    })
+      .exec(function (err, trip) {
+        next(err, trip);
+      });
+  }
 };
