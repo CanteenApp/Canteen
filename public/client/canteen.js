fdd7b57c3638fc107ed1cbc6d1fc20998ec6f486
Ryan Haase <ryandhaase@gmail.com>
2016-04-13 21:03:02 -0400

    Commented out un-minified versions of JS

--- public/client/canteen.js
+++ public/client/canteen.js
@@ -17,12 +17,12 @@ angular.module('canteen', [
     .state('tripForm', {
       url: '/trip-form',
       templateUrl: 'client/trip-form/tripForm.html',
-      controller : 'tripForm'
+      controller: 'tripForm'
     })
     .state('listView', {
       url: '/list/{listName}',
       templateUrl: 'client/list-view/listView.html',
-      controller: 'listView'
+      controller: 'listView',
     });
     // .state('tripForm.lists', {
     //   url: "/lists",
