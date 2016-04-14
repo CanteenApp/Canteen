e004ed0e42e228cfbab601e7268d4a86ffcac8dd
Jak Buttarazzi <jakrazzi@gmail.com>
2016-04-14 16:49:38 -0500

    Finish OAuth sessions and user creation (#108)
    * Add Purest to package.json
    
    * Handle callback route from OAuth
    
    * Refactor userController for OAuth
    
    * Refactor userModel for OAuth

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,18 +2,23 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
-  username: {
-    type: String, 
-    index: {unique:true}
-  },
-  password: String,
+  // username: {
+  //   type: String, 
+  //   index: {unique:true}
+  // },
+  id: String,
+  email: String,
+  given_name: String,
+  family_name: String,
+  trip: String
+  // password: String,
 });
 
-userSchema.pre('save', function (next) {
-  return bcrypt.hash(this.password, 5, function(err, hash) {
-    this.password = hash;
-    next();
-  });
-});
+// userSchema.pre('save', function (next) {
+//   return bcrypt.hash(this.password, 5, function(err, hash) {
+//     this.password = hash;
+//     next();
+//   });
+// });
 
 module.exports = mongoose.model('User', userSchema);
