e0c74f4620b5e9a011a9b037d5054b873de7bef2
Ryan Haase <ryandhaase@gmail.com>
2016-04-14 15:16:23 -0400

    Initial Trip Overview page:
    * tripCtrl - pulling mocked data from tripData.js
    * tripView - Map & Trip details
    * Added routes to canteen.js

--- public/client/trip/tripCtrl.js
+++ public/client/trip/tripCtrl.js
@@ -1,7 +1,10 @@
 angular.module('canteen.trip', [])
 .controller('tripCtrl', [
   '$scope',
-  function ($scope) {
-    $scope.data = {};
+  'trip',
+  function ($scope, trip) {
+    $scope.trip = trip;
+
+    // add call to get trip once factory method is available
   },
 ]);
