80ee6e96eb4364b3ab414bd808207ea1ecda08c6
Todd Bontrager <bontragertodd@gmail.com>
2016-04-15 17:13:49 -0400

    Remove Members and Create trip from nav bar

--- public/client/navbar/navBar.js
+++ public/client/navbar/navBar.js
@@ -2,7 +2,27 @@ angular.module('canteen.navBar', [])
 
 .controller('navBar', [
   '$scope',
-  function ($scope) {
+  '$location',
+  function ($scope, $location) {
     $scope.listName = '';
+    $scope.location = $location.path();
+
+    $scope.isOnLandingPage = function () {
+      if ($location.path() === '/landing-page') {
+        console.log('on LP');
+        return true;
+      } else {
+        console.log('not on LP');
+        return false;
+      }
+    };
+
+
+    $scope.logOut = function () {
+    //break session
+    //redirect to landing page
+    //route request to back end
+    };
+  //$scope.isOnLandingPage();
   },
 ]);
