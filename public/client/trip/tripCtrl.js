a55e98389edb323adcb5defc95289e00bd200a61
Jak Buttarazzi <JakRazzi@gmail.com>
2016-04-16 13:56:23 -0500

    Make dates display in human-friendly way

--- public/client/trip/tripCtrl.js
+++ public/client/trip/tripCtrl.js
@@ -6,11 +6,14 @@ angular.module('canteen.trip', [])
   function ($scope, trip, NgMap) {
     trip.getTrip(function(tripData){
       $scope.trip = tripData;
+      $scope.dates = {
+        start: moment($scope.trip.dates.start).format('MMM Do, YYYY'),
+        end: moment($scope.trip.dates.end).format('MMM Do, YYYY')
+      };
     });
     $scope.color = {
       colors: ['red', 'blue', 'purple', 'green', 'orange'],
     };
-
     // NgMap.getMap().then(function (map) {
     // });
 
